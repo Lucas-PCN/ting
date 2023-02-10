47fde6d1d5212dbc8eb4e7f8b0bcf3348604e967
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-02-10 09:50:53 -0300

    Feat: add req 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_read = txt_importer(path_file)
+
+    for i in range(instance.__len__()):
+        dict_item = instance.search(i)
+        if dict_item["nome_do_arquivo"] == path_file:
+            return None
+
+    dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_read),
+        "linhas_do_arquivo": file_read,
+    }
+
+    instance.enqueue(dict)
+    print(dict)
 
 
 def remove(instance):

65dde629a3c5926936b884b8f8337c7373f610be
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-02-10 09:35:25 -0300

    Feat: add req 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação!"""
+    if not path_file.endswith('.txt'):
+        return print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, mode='r') as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)

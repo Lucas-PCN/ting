d778937653e6525a2668f9850aed8dcc945017a7
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-02-10 10:14:11 -0300

    Feat: add req 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    word_occurrences = []
+
+    for data in range(instance.__len__()):
+        file_search = instance.search(data)
+
+        for index, line in enumerate(file_search["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                word_occurrences.append({"linha": index + 1})
+
+        if len(word_occurrences) > 0:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_search["nome_do_arquivo"],
+                    "ocorrencias": word_occurrences,
+                }
+            )
+
+    return result
 
 
 def search_by_word(word, instance):
